2cf2ca96f4dc37f0f291df50ec956e7936d46b71
Ngoc.tv <tongoc92@gmail.com>
2016-10-05 00:18:29 +0700

    seeds the administration area

---
+++ home-stay/database/migrations/2016_10_04_171033_create_provinces_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProvincesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provinces', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->string('name');
+            $table->string('prefix');
+            $table->string('district_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('provinces');
+    }
+}
